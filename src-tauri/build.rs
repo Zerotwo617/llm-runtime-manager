fc0688df753a2fa5125213e55c74cf5dcabf86a1
Zerotwo6 <Zerotwo61708@gmail.com>
2026-06-29 20:26:47 +0800

    fix: use resource-friendly windows icon

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     #[cfg(windows)]
-    embed_resource::compile("app-icon.rc", embed_resource::NONE);
+    let _ = embed_resource::compile("app-icon.rc", embed_resource::NONE);
 
     tauri_build::build();
 }
